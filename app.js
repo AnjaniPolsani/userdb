1db887be84789e2e7f1863b4bee9457915489b5e
AnjaniPolsani <137593183+AnjaniPolsani@users.noreply.github.com>
2023-08-10 17:48:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ const path = require("path");
 const databasePath = path.join(__dirname, "userData.db");
 const app = express();
 app.use(express.json());
-const express = require("express");
-const { open } = require("sqlite");
-const sqlite3 = require("sqlite3");
-const path = require("path");
-const databasePath = path.join(__dirname, "userData.db");
-const app = express();
-app.use(express.json());
 const bcrypt = require("bcrypt");
 const initializeDbAndServer = async () => {
   try {
